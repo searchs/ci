51ee30337cbe6f62bf1762773827529f7349c241
Ola Ajibode <ola.ajibode@gmail.com>
2016-06-21 13:51:30 +0100

    Swapped license acceptance

--- provision.sh
+++ provision.sh
@@ -1,8 +1,9 @@
-#Setup Vagrant inspired machine
+#Setup test or dev environment using Vagrant
 sudo apt-get update ;
 sudo apt-get upgrade  && sudo apt-get dist-upgrade -y
 
-sudo add-apt-repository ppa:webupd8team/java
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer

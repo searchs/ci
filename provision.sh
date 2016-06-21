ceacaa4842a92db77d6a778314894b4e74f5cfae
Ola Ajibode <ola.ajibode@gmail.com>
2016-06-21 15:04:36 +0100

    Tasks added

--- provision.sh
+++ provision.sh
@@ -1,10 +1,18 @@
 #Setup test or dev environment using Vagrant
-#
+#TOOD: add user - ubuntu to machine
+#TODO:  fix permissions for  created user - ubuntu
 apt-get -y update ;
 apt-get -y upgrade  && sudo apt-get dist-upgrade -y
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install git-core zlib1g-dev libssl-dev libreadline5-dev libcurl4-openssl-dev libyaml-dev curl
 apt-get clean
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+\curl -sSL https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
+rvm reload
+rvm install ruby-2.3.0
+rvm list
+ruby -v
 apt-get install -y python-software-properties debconf-utils
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
@@ -13,12 +21,7 @@ apt-get install -y oracle-java8-installer
 apt-get install -y curl
 java -version
 #sudo apt-get install oracle-java8-set-default
-command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-\curl -sSL https://get.rvm.io | bash -s stable
-source /home/vagrant/.rvm/scripts/rvm
-rvm reload
-rvm install ruby-2.3.0
-rvm list
-ruby -v
+apt-get autoclean -y
+apt-get autoremove -y
 
 exit

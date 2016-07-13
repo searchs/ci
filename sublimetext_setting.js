f2acaaf90147b1fb418ca5770cfe0f5019204b8f
Ola Ajibode <ola.ajibode@gmail.com>
2016-07-13 11:32:46 +0100

    create sublimetext3 settings

---
+++ sublimetext_setting.js
@@ -0,0 +1,28 @@
+{
+	"color_scheme": "Packages/User/SublimeLinter/iPlastic (SL).tmTheme",
+	"font_face": "Consolas",
+	"font_size": 16,
+	"ignored_packages":
+	[
+		"All Autocomplete",
+		"AngularJS Snippets",
+		"JavaScript Completions",
+		"Vintage"
+	],
+	"highlight_line": true,
+    "highlight_modified_tabs": true,
+    "line_padding_bottom": 1,
+    "line_padding_top": 1,
+    "rulers":
+    [
+        80
+    ],
+    "scroll_past_end": true,
+    "tab_size": 4,
+    "tab_completion": false,
+    "theme": "Soda Light.sublime-theme",
+    "translate_tabs_to_spaces": true,
+    "trim_trailing_white_space_on_save": true,
+    "vintage_start_in_command_mode": true,
+    "word_wrap": true
+}
